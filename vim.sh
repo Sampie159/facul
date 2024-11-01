874b9b39185cfeac958aca1e4b8eaa1b0e608f71
Sampie159 <38163547+Sampie159@users.noreply.github.com>
2024-11-01 06:00:24 -0300

    ca

--- vim.sh
+++ vim.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-git clone --depth=1 -b release-0.10 --single-branch https://github.com/neovim/neovim.git 
+git clone --depth=1 -b v0.10.2 --single-branch https://github.com/neovim/neovim.git 
 cd neovim
 make CMAKE_BUILD_TYPE=Release CMAKE_INSTALL_PREFIX=~/
 make install
 ln -sf ~/facul/nvim ~/.config
+ln -sf ~/facul/tmux ~/.config
